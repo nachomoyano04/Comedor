f22ca3fb8a26b19d8d3dcc8c76eaafda40268883
Nacho <nachomoyag@gmail.com>
2025-10-13 15:06:02 -0300

    Time zone config

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ const pool = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    timezone: "-3:00"
 });
 export default pool;
